3aa1888551e935e92fc1c60367c4e180dba1a793
Julia Jakob <juliajakob2106@gmail.com>
2026-09-13 22:12:26 -0400

    add placeholders for pages

---
+++ src/pages/Blog.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Blog() {
+    return <div className="viewContent">Blog Page Content</div>;
+}
---
+++ src/pages/Experience.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Experience() {
+    return <div className="viewContent">Experience Page Content</div>;
+}
